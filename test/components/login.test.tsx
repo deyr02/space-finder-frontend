0a4fca75c6869539ce6dbc5acb361dabc4f0fb17
Rajib Dey <DEYR02@myunitec.ac.nz>
2021-08-07 14:58:13 +1200

    test hooks and mocks

---
+++ test/components/login.test.tsx
@@ -0,0 +1,52 @@
+import ReactDOM from 'react-dom';
+import Login from '../../src/components/login';
+describe('Login component test suite', ()=>{
+
+    //test Hooks
+    // beforeAll(()=>{
+    //     console.log('before all');
+    // })
+    // afterAll(()=>{
+    //     console.log('after all');
+    // })
+
+
+    // beforeEach(()=>{
+    //     console.log('before each');
+    // })
+    // afterEach(()=>{
+    //     console.log('after each');
+    // })
+//-------------------------------------------------------------//
+    let container:HTMLDivElement;
+    //jest mock
+    const authServiceMock = {
+        login: jest.fn() //jest mock funtion
+    }
+
+    const setUserMock = jest.fn();
+    const setUserAttributeMock = jest.fn();
+    //------
+
+    beforeEach(()=>{
+        container = document.createElement('div');
+        document.appendChild(container);
+        ReactDOM.render(
+            <Login authservice={authServiceMock as any} setUser= {setUserMock} setUserAttributes={setUserAttributeMock}/>
+            , container)
+
+    })
+    afterEach(()=>{
+        document.body.removeChild(container);
+        container.remove();
+        jest.clearAllMocks();
+    })
+
+
+
+
+
+    test('initial test', ()=>{
+        expect(true).toBeTruthy();
+    })
+})
