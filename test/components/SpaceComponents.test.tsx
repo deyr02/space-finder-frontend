402841dee414eac6cd859bfefb5ac54cbc440d51
Rajib Dey <DEYR02@myunitec.ac.nz>
2021-08-09 13:19:40 +1200

    Testing Multiple Suites

--- test/components/SpaceComponents.test.tsx
+++ test/components/SpaceComponents.test.tsx
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/react';
 import ReactDOM from 'react-dom';
 import SpaceComponent from '../../src/components/spaces/SpaceComponent';
 import { SpaceObject } from '../../src/model/Model';
@@ -8,35 +9,72 @@ import { SpaceObject } from '../../src/model/Model';
 describe('Space component test suite', ()=>{
     let container: HTMLDivElement;
     const reserveSpaceMock = jest.fn();
-    const spaceObject:SpaceObject = {spaceId:'123', name:'someName', location:"someLocation", photoUrl:''}
+    const spaceObject:SpaceObject = {spaceId:'123', name:'someName', location:"someLocation", photoUrl:'some.url'}
+    const spaceObjectWithoutUrl:SpaceObject = {spaceId:'123', name:'someName', location:"someLocation"};
 
+    function cleanUpTest(){
+        document.body.removeChild(container);
+        container.remove();
+        jest.clearAllMocks();
+    }
+    
+    function setUpTests(element: React.FunctionComponentElement<any>){
+        container = document.createElement('div');
+        document.body.appendChild(container)
+        ReactDOM.render(element,container)
+    }
     describe('tests with photo URL', ()=>{
 
         beforeEach(()=>{
-            container = document.createElement('div');
-            document.body.appendChild(container)
-            ReactDOM.render(<SpaceComponent 
-                spaceObject = {spaceObject}
-                reserveSpace={reserveSpaceMock}
-               
-            />,
-            container)
+            setUpTests(<SpaceComponent spaceObject = {spaceObject} reserveSpace={reserveSpaceMock}/>);
         })
 
-        test('basic rendering', ()=>{
+        test('show image correctly', ()=>{
+            const image = container.querySelector('img');
+            expect(image!).toBeInTheDocument();
+
+            expect(image!.src).toBe('http://localhost/some.url');
+        })
+        
+        test('show labels correclty', ()=>{
+            const label = container.querySelectorAll('label');
+            expect(label[0]).toHaveTextContent('someName')
+            expect(label[1]).toHaveTextContent('123')
+            expect(label[2]).toHaveTextContent('someLocation')
+        })
+        test('reserve spaces', ()=>{
+            const button = container.querySelector('button');
+            fireEvent.click(button!);
+            expect(reserveSpaceMock).toBeCalledWith('123');
 
         })
 
         afterEach(()=>{
-            document.body.removeChild(container);
-            container.remove();
-            jest.clearAllMocks();
+            cleanUpTest();
         })
 
     })
 
     describe('tests without photo URL', ()=>{
+     beforeEach(()=>{
+            setUpTests(<SpaceComponent spaceObject = {spaceObjectWithoutUrl} reserveSpace={reserveSpaceMock}/>);
+           
+        })
 
+
+        test('show image correctly', ()=>{
+            const image = container.querySelector('img');
+            expect(image!).toBeInTheDocument();
+
+            expect(image!.src).toBeFalsy();
+        })
+
+           afterEach(()=>{
+            cleanUpTest();
+        })
     })
+
+
+
 
 }); 
